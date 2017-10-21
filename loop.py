c5b95716c589800dcd158a5cf718c43c5f559483
manibharath27 <32952588+manibharath27@users.noreply.github.com>
2017-10-21 15:37:31 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      16cse022
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse022 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+ for letter in 'Python':
+   print 'Current Letter :', letter
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:
+   print 'Current fruit :', fruit
+print "Good bye!"
+count = 0
+while (count < 9):
+   print 'The count is:', count
+   count = count + 1
+print "Good bye!"
+
+if __name__ == '__main__':
+    main()
